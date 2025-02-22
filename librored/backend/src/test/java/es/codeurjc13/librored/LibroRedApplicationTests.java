2a7b3d48d4eee932468913bc0142451415afdd17
Ana <maramedinacrespo@gmail.com>
2025-02-22 13:07:49 +0100

    Add files to commit even if there are no changes
    I don't know why this happened

---
+++ librored/backend/src/test/java/es/codeurjc13/librored/LibroRedApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc13.librored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroRedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
