3c75de35d8fb6916d86cb0551dc4b66804c03ba5
Ana <maramedinacrespo@gmail.com>
2025-02-26 22:11:46 +0100

    Add again librored folder with its content

---
+++ librored/backend/src/main/java/es/codeurjc13/librored/model/Book.java
@@ -0,0 +1,93 @@
+package es.codeurjc13.librored.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+
+    @Enumerated(EnumType.STRING)
+    private Genre genre;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String cover_pic;
+
+    @ManyToOne
+    private User owner;
+
+    public enum Genre {
+        Fiction, Non_Fiction, Mystery_Thriller, SciFi_Fantasy, Romance, Historical_Fiction, Horror
+    }
+
+    public Book() {}
+
+    public Book(String title, String author, Genre genre, String description, String cover_pic, User owner) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.description = description;
+        this.cover_pic = cover_pic;
+        this.owner = owner;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCover_pic() {
+        return cover_pic;
+    }
+
+    public void setCover_pic(String cover_pic) {
+        this.cover_pic = cover_pic;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
