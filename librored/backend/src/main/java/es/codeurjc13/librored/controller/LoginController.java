13aba5a0b0b786d2c976473ee934a277ea95585a
Ana <maramedinacrespo@gmail.com>
2025-03-01 12:49:33 +0100

    Create Controller classes for login and register

---
+++ librored/backend/src/main/java/es/codeurjc13/librored/controller/LoginController.java
@@ -0,0 +1,9 @@
+package es.codeurjc13.librored.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+public class LoginController {
+}
---
+++ librored/backend/src/main/java/es/codeurjc13/librored/controller/RegisterController.java
@@ -0,0 +1,8 @@
+package es.codeurjc13.librored.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class RegisterController {
+}
