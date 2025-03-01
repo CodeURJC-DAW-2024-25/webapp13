3eadc7588c8fe200068b4cb2fa47abcb6a2cd999
Ana <maramedinacrespo@gmail.com>
2025-03-02 00:05:49 +0100

    Add LoginWebController. Remove imported LoginController.java

--- librored/backend/src/main/java/es/codeurjc13/librored/controller/LoginController.java
+++
@@ -1,9 +0,0 @@
-package es.codeurjc13.librored.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-
-public class LoginController {
-}
