999da91408e687a66e9c73066e6f81985e73153b
Ana <maramedinacrespo@gmail.com>
2025-03-05 21:46:00 +0100

    Remove unused call

--- librored/backend/src/main/java/es/codeurjc13/librored/repository/BookRepository.java
+++ librored/backend/src/main/java/es/codeurjc13/librored/repository/BookRepository.java
@@ -12,11 +12,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Query("SELECT b.genre, COUNT(b) FROM Book b GROUP BY b.genre")
     List<Object[]> countBooksByGenre();
 
-    @Query("SELECT b FROM Book b WHERE b.owner.id = :ownerId")
-    List<Book> findByOwnerId(@Param("ownerId") Long ownerId);
-
     @Query("SELECT b FROM Book b WHERE b.owner.id = :ownerId AND " + "b.id NOT IN (SELECT l.book.id FROM Loan l WHERE l.status = 'Active')")
     List<Book> findAvailableBooksByOwnerId(@Param("ownerId") Long ownerId);
-
 
 }
