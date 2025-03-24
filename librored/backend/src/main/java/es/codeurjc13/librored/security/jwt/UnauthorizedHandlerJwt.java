d7c79967ace50fe91c91ba40f1fdfffdf5abe84d
Ana <maramedinacrespo@gmail.com>
2025-03-24 15:11:59 +0100

    Simplify UnauthorizedHandlerJwt.java

--- librored/backend/src/main/java/es/codeurjc13/librored/security/jwt/UnauthorizedHandlerJwt.java
+++ librored/backend/src/main/java/es/codeurjc13/librored/security/jwt/UnauthorizedHandlerJwt.java
@@ -1,19 +1,20 @@
 package es.codeurjc13.librored.security.jwt;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class UnauthorizedHandlerJwt implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(UnauthorizedHandlerJwt.class);
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex)
@@ -22,12 +23,8 @@ public class UnauthorizedHandlerJwt implements AuthenticationEntryPoint {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-        Map<String, Object> body = new HashMap<>();
-        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-        body.put("error", "Unauthorized");
-        body.put("message", ex.getMessage());
-        body.put("path", request.getServletPath());
+        logger.info("Unauthorized error: {}", ex.getMessage());
 
-        new ObjectMapper().writeValue(response.getOutputStream(), body);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "message: %s, path: %s".formatted(ex.getMessage(), request.getServletPath()));
     }
 }
