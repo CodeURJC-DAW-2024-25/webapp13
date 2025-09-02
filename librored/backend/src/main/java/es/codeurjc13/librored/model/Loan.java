dd1a8a2681630a7336601ee5ed73fe1b5dd1914f
Ana <maramedinacrespo@gmail.com>
2025-09-02 10:22:19 +0200

    ID generation: GenerationType.AUTO → GenerationType.IDENTITY (proper for MySQL)

--- librored/backend/src/main/java/es/codeurjc13/librored/model/Loan.java
+++ librored/backend/src/main/java/es/codeurjc13/librored/model/Loan.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 @Entity
 public class Loan {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
