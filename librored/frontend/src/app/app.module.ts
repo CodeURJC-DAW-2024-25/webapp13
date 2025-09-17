82e2784a855d242fcc4c274387830270e5b159f7
Ana <maramedinacrespo@gmail.com>
2025-09-17 18:16:14 +0200

    Register new home and register components

--- librored/frontend/src/app/app.module.ts
+++ librored/frontend/src/app/app.module.ts
@@ -7,23 +7,27 @@ import { routing } from "./app.routing";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
 import { BookDetailComponent } from "./components/books/book-detail.component";
 import { BookListComponent } from "./components/books/book-list.component";
 import { BookFormComponent } from "./components/books/book-form.component";
 import { LoanListComponent } from "./components/loans/loan-list.component";
 import { LoanFormComponent } from "./components/loans/loan-form.component";
 import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
 import { AuthInterceptor } from "./interceptors/auth.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     BookDetailComponent,
     BookListComponent,
     BookFormComponent,
     LoanListComponent,
     LoanFormComponent,
     LoginComponent,
+    RegisterComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule, routing, NgbModule],
   providers: [
